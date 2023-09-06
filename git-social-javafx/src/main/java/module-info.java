aa729cb379fdc95f8d52f905e29cbbf1077f23db
Heiffeng <suna235792@gmail.com>
2023-09-06 10:05:03 +0800

    init projects

---
+++ git-social-javafx/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module site.achun.git.social {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens site.achun.git.social to javafx.fxml;
+    exports site.achun.git.social;
+}
