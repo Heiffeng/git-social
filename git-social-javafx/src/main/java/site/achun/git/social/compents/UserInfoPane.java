470beb06fb1e9a5eedc48d291251103328402835
Heiffeng <suna235792@gmail.com>
2023-10-08 15:53:38 +0800

    Update UserInfoPane.java

--- git-social-javafx/src/main/java/site/achun/git/social/compents/UserInfoPane.java
+++ git-social-javafx/src/main/java/site/achun/git/social/compents/UserInfoPane.java
@@ -2,10 +2,19 @@ package site.achun.git.social.compents;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import site.achun.git.social.data.User;
+import site.achun.git.social.local.CurrentUser;
+
+import java.io.IOException;
 
 public class UserInfoPane extends VBox {
 
     public UserInfoPane(){
-        this.getChildren().add(new Label("UserInfo"));
+        try {
+            User user = CurrentUser.getUser();
+            this.getChildren().add(new Label("Name:" + user.getNickname()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
